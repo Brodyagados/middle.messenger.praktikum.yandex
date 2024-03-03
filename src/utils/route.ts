d5b1c51ab5d4ba1eefd1baeb294480f804db58eb
Igor Mezentsev <schumm@mail.ru>
2024-03-03 16:18:41 +0500

    Добавил описание

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -2,8 +2,11 @@ import Handlebars from 'handlebars';
 
 import * as Pages from '../pages';
 
+/** Объект "Страница" */
 class Page {
+    /** ссылка на страницу */
     path: string;
+    /** шаблон страницы */
     template: string;
 
     constructor(path: string, template: string) {
@@ -11,6 +14,10 @@ class Page {
         this.template = template;
     };
 
+    /** 
+     * Метод перехода на страницу
+     * @param {object} routeData данные для перехода на страницу
+     */
     route(routeData: object = {}) {
         const app = document.getElementById('app');
         const template = Handlebars.compile(this.template);
@@ -19,10 +26,16 @@ class Page {
     }
 }
 
+/** Страница */
 export const page = {
+    /** Страница "Чаты" */
     home: new Page('/', 'home'),
+    /** Страница "Авторизация" */
     login: new Page('/login', 'login'),
+    /** Страница "Регистрация" */
     signIn: new Page('/sign-in', 'signIn'),
+    /** Страница "Ошибка 404" */
     notFound: new Page('/not-found', Pages.NotFoundErrorPage),
+    /** Страница "Ошибка 500" */
     internalServerError: new Page('/internal-server-error', Pages.InternalServerErrorPage)
 }
