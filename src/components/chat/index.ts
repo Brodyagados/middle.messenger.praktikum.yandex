cf9eee2e49994eae57ed9f4453bc24121cd06821
Igor Mezentsev <schumm@mail.ru>
2024-03-05 18:11:16 +0500

    Завершен список чатов

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,4 +1,5 @@
 export { ChatList } from './chat-list';
+export { ChatListAccountLink } from './chat-list-account-link';
 export { ChatListHeader } from './chat-list-header';
 export { ChatListItem } from './chat-list-item';
 export { ChatListSearch } from './chat-list-search';
