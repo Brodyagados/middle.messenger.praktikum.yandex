de94fba29f57a5d8f4983bd273a3b639246395b5
Igor Mezentsev <schumm@mail.ru>
2024-03-10 14:43:18 +0500

    Вынес страницы с настройками пользователя в отдельную папку

---
+++ src/pages/user-setting/index.ts
@@ -0,0 +1,3 @@
+export { UserSettingPage } from './user-setting-page';
+export { UserSettingEditPage } from './user-setting-edit-page';
+export { UserSettingChangePasswordPage } from './user-setting-change-password-page';
