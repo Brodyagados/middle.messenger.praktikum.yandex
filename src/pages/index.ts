7a26a0ce7892669b583d2b15a453f32fe2b9bcc8
Igor Mezentsev <schumm@mail.ru>
2024-03-10 13:35:11 +0500

    Добавил страницу "Редактирование настроек пользователя"

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { ChatPage } from './chat-page';
 export { UserSettingPage } from './user-setting-page';
+export { UserSettingEditPage } from './user-setting-edit-page';
 export * from './account';
 export * from './error';
