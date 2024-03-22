078161bd0df5aa8e0571a0b2e30b9bd6a97cad7c
Mezentsev Igor <schumm@mail.ru>
2024-03-22 14:26:53 +0500

    Переделал шаблон ссылки

--- src/components/base/link/index.ts
+++ src/components/base/link/index.ts
@@ -1,2 +1,23 @@
+import Block from '../../../utils/Block';
 import './link.scss';
-export { default as Link } from './link.hbs?raw';
+import template from './link.hbs?raw';
+import renderTemplate from '../../../utils/render-template';
+
+
+interface ILink {
+    url?: string,
+    class?: string,
+    page?: string,
+    text: string
+}
+
+export class Link extends Block {
+    constructor(props: ILink) {
+        super('div', props);
+        this._props = props;
+    }
+
+    render(): string {
+        return renderTemplate(template, this._props);
+    }
+}
