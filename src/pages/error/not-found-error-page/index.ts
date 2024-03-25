f1d5451ff147182eebf252fae740f8949cf7141c
Igor Mezentsev <schumm@mail.ru>
2024-03-25 05:17:37 +0500

    Начало работы над переработкой страницы "404"

--- src/pages/error/not-found-error-page/index.ts
+++ src/pages/error/not-found-error-page/index.ts
@@ -1,1 +1,21 @@
-export { default as NotFoundErrorPage } from './not-found-error-page.hbs?raw';
+import Block from '../../../utils/Block';
+import template from './not-found-error-page.hbs?raw';
+import renderTemplate from '../../../utils/render-template';
+
+
+interface INotFoundErrorPage {
+    title: string,
+    description: string,
+    homeLink: string
+}
+
+export class NotFoundErrorPage extends Block {
+    constructor(props: INotFoundErrorPage) {
+        super('div', props);
+        this._props = props;
+    }
+
+    render(): string {
+        return renderTemplate(template, this._props);
+    }
+}
