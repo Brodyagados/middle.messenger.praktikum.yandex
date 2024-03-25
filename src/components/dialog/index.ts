ebb10afdbf3b1183a84acf3967a86552251a5fce
Mezentsev Igor <schumm@mail.ru>
2024-03-25 14:23:41 +0500

    Удалил лишний код

--- src/components/dialog/index.ts
+++ src/components/dialog/index.ts
@@ -1,5 +1,4 @@
 export { DialogHeader } from './dialog-header';
 export { DialogMain } from './dialog-main';
 export { DialogFooter } from './dialog-footer';
-export { ErrorDialog } from './error-dialog';
 export * from './user-setting-dialog';
