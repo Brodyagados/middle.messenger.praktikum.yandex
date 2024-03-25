90faa6884a903077a5dc0acce1cb038b39045fa0
Mezentsev Igor <schumm@mail.ru>
2024-03-25 08:16:26 +0500

    Переделал страницу "5**"

--- src/pages/error/internal-server-error-page/index.ts
+++ src/pages/error/internal-server-error-page/index.ts
@@ -1,1 +1,13 @@
-export { default as InternalServerErrorPage } from './internal-server-error-page.hbs?raw';
+import { Link, PageDescription, PageTitle } from '../../../components';
+import { ErrorPage } from '..';
+
+
+const context = {
+    title: new PageTitle({ text: '500' }).render(),
+    description: new PageDescription({ text: 'Внутренная ошибка сервера' }).render(),
+    homeLink: new Link({ page: '/', text: 'Назад к чатам' }).render()
+};
+
+export const InternalServerErrorPage = () => (
+    new ErrorPage(context).render()
+);
