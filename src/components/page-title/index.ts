990b9dbe03a6b8136ffb11abc2cbbcd37a5ca9ac
Igor Mezentsev <schumm@mail.ru>
2024-03-25 05:16:47 +0500

    Переделал компоненту "Заголовок страницы"

--- src/components/page-title/index.ts
+++ src/components/page-title/index.ts
@@ -1,2 +1,20 @@
+import Block from '../../utils/Block';
 import './page-title.scss';
-export { default as PageTitle } from './page-title.hbs?raw';
+import template from './page-title.hbs?raw';
+import renderTemplate from '../../utils/render-template';
+
+
+interface IPageTitle {
+    text: string
+}
+
+export class PageTitle extends Block {
+    constructor(props: IPageTitle) {
+        super('div', props);
+        this._props = props;
+    }
+
+    render(): string {
+        return renderTemplate(template, this._props);
+    }
+}
