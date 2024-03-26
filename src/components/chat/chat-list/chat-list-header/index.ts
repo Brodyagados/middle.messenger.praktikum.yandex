7bcb3e47dada32c351c34cef90b9254d21e45b20
Igor Mezentsev <schumm@mail.ru>
2024-03-26 21:24:00 +0500

    Переделал компоненту "Заглавная часть списка чатов"

--- src/components/chat/chat-list/chat-list-header/index.ts
+++ src/components/chat/chat-list/chat-list-header/index.ts
@@ -1,2 +1,21 @@
 import './chat-list-header.scss';
-export { default as ChatListHeader } from './chat-list-header.hbs?raw';
+import template from './chat-list-header.hbs?raw';
+import renderTemplate from '../../../../utils/render-template';
+import Block from '../../../../utils/Block';
+import { ChatListSearch } from '..';
+
+
+export class ChatListHeader extends Block {
+    constructor(props: never) {
+        super('div', props);
+        this._props = props;
+    }
+
+    render(): string {
+        const context = {
+            search: new ChatListSearch({ placeholder: 'Поиск', name: 'search' }).render()
+        };
+
+        return renderTemplate(template, context);
+    }
+}
