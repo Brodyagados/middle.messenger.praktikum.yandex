f86922083f2000c190ac61eaea4ead73c50413f9
Mezentsev Igor <schumm@mail.ru>
2024-03-26 15:16:22 +0500

    Переделал компоненту "Диалог чата"

--- src/components/chat/chat-dialog/index.ts
+++ src/components/chat/chat-dialog/index.ts
@@ -1,17 +1,30 @@
-import Handlebars from 'handlebars';
-
 import './chat-dialog.scss';
-import additionalButtonIconSrc from '../../../assets/icons/vertical-dotes.svg'
-import attachmentButtonIconSrc from '../../../assets/icons/attachment.svg'
-import sendButtonIconSrc from '../../../assets/icons/arrow-right.svg'
-export { default as ChatDialog } from './chat-dialog.hbs?raw';
-export { ChatDialogMessage } from './chat-dialog-message';
-export { ChatDialogInput } from './chat-dialog-input';
+import Block from '../../../utils/Block';
+import template from './chat-dialog.hbs?raw';
+import renderTemplate from '../../../utils/render-template';
+import { DialogFooter, DialogHeader, DialogMain } from '../../../components';
 
-Handlebars.registerHelper('chatImages', (options) => {
-    return options.fn({
-        additionalButtonIconSrc,
-        attachmentButtonIconSrc,
-        sendButtonIconSrc
-    })
-});
+
+interface IChatDialog {
+    header: string[],
+    main: string[],
+    footer: string[]
+}
+
+export class ChatDialog extends Block {
+    constructor(props: IChatDialog) {
+        super('div', props);
+        this._props = props;
+    }
+
+    render(): string {
+        const { header, main, footer } = this._props;
+        const context = {
+            header: new DialogHeader({ content: Object.values(header).join('') }).render(),
+            main: new DialogMain({ content: Object.values(main).join('') }).render(),
+            footer: new DialogFooter({ content: Object.values(footer).join('') }).render()
+        };
+
+        return renderTemplate(template, context);
+    }
+}
