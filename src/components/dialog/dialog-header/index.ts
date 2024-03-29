df01760206208326be6287a4ab6c41b4bb293e84
Igor Mezentsev <schumm@mail.ru>
2024-03-29 16:49:46 +0500

    Переделаны компоненты диалога

--- src/components/dialog/dialog-header/index.ts
+++ src/components/dialog/dialog-header/index.ts
@@ -2,7 +2,18 @@ import Block from '../../../utils/Block';
 import './dialog-header.scss';
 import template from './dialog-header.hbs?raw';
 
+interface IDialogHeader {
+  content: Block[]
+}
+
 export class DialogHeader extends Block {
+  constructor(props: IDialogHeader) {
+    super({
+      ...props,
+      attr: { class: 'dialog__header'}
+    });
+  }
+
   render() {
     return this.compile(template, this._props);
   }
--- src/components/dialog/dialog-main/index.ts
+++ src/components/dialog/dialog-main/index.ts
@@ -2,7 +2,18 @@ import Block from '../../../utils/Block';
 import './dialog-main.scss';
 import template from './dialog-main.hbs?raw';
 
+interface IDialogMain {
+  content: Block[]
+}
+
 export class DialogMain extends Block {
+  constructor(props: IDialogMain) {
+    super({
+      ...props,
+      attr: { class: 'dialog__main'}
+    });
+  }
+  
   render() {
     return this.compile(template, this._props);
   }
