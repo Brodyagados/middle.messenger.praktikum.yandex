161a7be33150a73632d7cd60d2d120896d8cbe49
Igor Mezentsev <schumm@mail.ru>
2024-03-29 18:10:14 +0500

    Переделана компонента "TextBox"

--- src/components/textbox/index.ts
+++ src/components/textbox/index.ts
@@ -1,33 +1,31 @@
 import Block from '../../utils/Block';
 import './textbox.scss';
 import template from './textbox.hbs?raw';
-import renderTemplate from '../../utils/render-template';
 import { Input } from '..';
-import { IInput } from '../base/input';
 import { Validation } from '../../utils/ValidationType';
+import { IInput } from '../base/input';
 
-interface ITextBox extends IInput {
-    class?: string,
+interface ITextBox {
     label?: string,
-    validationType?: string
+    inputProps: IInput,
+    attr: {
+      class?: string
+    }
 }
 
 export class TextBox extends Block {
   constructor(props: ITextBox) {
-    super(props);
-    this._props = props;
+    const { label, inputProps: { attr: { validation } }, attr: { class: cssClass = '' } } = props;
+    
+    super({
+      label,
+      error: validation ? Validation.getByType(validation)?.message : '',
+      input: new Input({ ...props.inputProps }),
+      attr: { class: `textbox ${cssClass}`}
+    });
   }
 
-  render(): string {
-    const { class: cssClass, label, ...inputProps } = this._props;
-    const error = Validation.getByType(inputProps.validationType)?.message;
-    const context = {
-      class: cssClass,
-      label,
-      error,
-      input: new Input(inputProps as IInput).getContentAsString(),
-    };
-
-    return renderTemplate(template, context);
+  render() {
+    return this.compile(template, this._props);
   }
 }
