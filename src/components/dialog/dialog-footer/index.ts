20deb1cfdf61a3a96672dd7dcae057c9f9b60688
Igor Mezentsev <schumm@mail.ru>
2024-03-29 15:46:47 +0500

    Переделал компоненты диалога

--- src/components/dialog/dialog-footer/index.ts
+++ src/components/dialog/dialog-footer/index.ts
@@ -1,19 +1,9 @@
 import Block from '../../../utils/Block';
 import './dialog-footer.scss';
 import template from './dialog-footer.hbs?raw';
-import renderTemplate from '../../../utils/render-template';
-
-interface IDialogFooter {
-    content: string
-}
 
 export class DialogFooter extends Block {
-  constructor(props: IDialogFooter) {
-    super(props);
-    this._props = props;
-  }
-
-  render(): string {
-    return renderTemplate(template, this._props);
+  render() {
+    return this.compile(template, this._props);
   }
 }
