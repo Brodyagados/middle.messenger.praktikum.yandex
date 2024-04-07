b1780cc72ddaa270a3bd74f266a58be8cbe9833e
Igor Mezentsev <schumm@mail.ru>
2024-04-07 22:05:19 +0500

    Вынес страницы в Enum

---
+++ src/constants/PagePath.ts
@@ -0,0 +1,8 @@
+export enum PAGE_PATH {
+    login = '/',
+    signUp = '/sign-up',
+    changePassword = '/change-password',
+    messenger = '/messenger',
+    notFound = '/not-found',
+    internalServerError = '/internal-server-error',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import Router from './utils/Router';
 import * as Pages from './pages';
+import { PAGE_PATH } from './constants/PagePath';
 
 document.addEventListener('DOMContentLoaded', () => {
   const router = new Router('#app');
 
   router
-    .use('/', Pages.LoginPage)
-    .use('/sign-up', Pages.SignUpPage)
-    .use('/change-password', Pages.ChangePasswordPage)
-    .use('/not-found', Pages.NotFoundErrorPage)
-    .use('/internal-server-error', Pages.InternalServerErrorPage)
+    .use(PAGE_PATH.login, Pages.LoginPage)
+    .use(PAGE_PATH.signUp, Pages.SignUpPage)
+    .use(PAGE_PATH.changePassword, Pages.ChangePasswordPage)
+    .use(PAGE_PATH.notFound, Pages.NotFoundErrorPage)
+    .use(PAGE_PATH.internalServerError, Pages.InternalServerErrorPage)
     .start();
 });
