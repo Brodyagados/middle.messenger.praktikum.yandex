5dba7a0457d3a6df2fbb055bfc17c6266492e327
Igor Mezentsev <schumm@mail.ru>
2024-04-07 20:59:08 +0500

    Добавил переход на страницу чатов со страниц с ошибкой

--- src/pages/error/internal-server-error-page/index.ts
+++ src/pages/error/internal-server-error-page/index.ts
@@ -1,12 +1,24 @@
 import { Link, PageDescription, PageTitle } from '../../../components';
 import { ErrorPage } from '..';
+import Router from '../../../utils/Router';
 
 export class InternalServerErrorPage extends ErrorPage {
   constructor() {
     super({
       title: new PageTitle({ text: '500' }),
       description: new PageDescription({ text: 'Внутренная ошибка сервера' }),
-      homeLink: new Link({ attr: { page: '/' }, text: 'Назад к чатам' }),
+      homeLink: new Link({ 
+        attr: { page: '/' }, 
+        text: 'Назад к чатам',
+        events: {
+          click: (event: Event) => {
+            event.preventDefault();
+            
+            const router = new Router('#app');
+            router.go('/messenger');
+          }
+        }
+      }),
     });
   }
 }
--- src/pages/error/not-found-error-page/index.ts
+++ src/pages/error/not-found-error-page/index.ts
@@ -1,12 +1,24 @@
 import { Link, PageDescription, PageTitle } from '../../../components';
 import { ErrorPage } from '..';
+import Router from '../../../utils/Router';
 
 export class NotFoundErrorPage extends ErrorPage {
   constructor() {
     super({
       title: new PageTitle({ text: '404' }),
       description: new PageDescription({ text: 'Страница не найдена' }),
-      homeLink: new Link({ attr: { page: '/' }, text: 'Назад к чатам' }),
+      homeLink: new Link({ 
+        attr: { page: '/' }, 
+        text: 'Назад к чатам',
+        events: {
+          click: (event: Event) => {
+            event.preventDefault();
+            
+            const router = new Router('#app');
+            router.go('/messenger');
+          }
+        }
+      }),
     });
   }
 }
