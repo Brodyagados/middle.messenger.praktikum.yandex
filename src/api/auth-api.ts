cb505090ed9809d9ee1d23a328a199279ac96427
Igor Mezentsev <schumm@mail.ru>
2024-04-08 20:57:13 +0500

    Стилистика AuthAPI

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -2,28 +2,28 @@ import ApiClient from '../utils/ApiClient';
 import { BaseAPI } from './base-api';
 
 class AuthAPI extends BaseAPI {
-    _authUrl: string;
+  _authUrl: string;
 
-    constructor() {
-        super();
-        this._authUrl = `${this._baseUrl}/auth`;
-    }
+  constructor() {
+    super();
+    this._authUrl = `${this._baseUrl}/auth`;
+  }
 
-    signUp(data: Record<string, unknown>) {
-        return ApiClient.post(`${this._authUrl}/signup`, { data });
-    }
+  signUp(data: Record<string, unknown>) {
+    return ApiClient.post(`${this._authUrl}/signup`, { data });
+  }
 
-    signIn(data: Record<string, unknown>) {
-        return ApiClient.post(`${this._authUrl}/signin`, { data });
-    }
+  signIn(data: Record<string, unknown>) {
+    return ApiClient.post(`${this._authUrl}/signin`, { data });
+  }
 
-    user() {
-        return ApiClient.get(`${this._authUrl}/user`);
-    }
+  user() {
+    return ApiClient.get(`${this._authUrl}/user`);
+  }
 
-    logout() {
-        return ApiClient.post(`${this._authUrl}/logout`);
-    }
+  logout() {
+    return ApiClient.post(`${this._authUrl}/logout`);
+  }
 }
 
 export default new AuthAPI();
