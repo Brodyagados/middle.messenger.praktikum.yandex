555757484bf3a2dfff91b953e1251caa53e8c29b
Mezentsev Igor <schumm@mail.ru>
2024-04-12 09:11:04 +0500

    Доработал HOC

--- src/utils/HOC.ts
+++ src/utils/HOC.ts
@@ -1,14 +1,20 @@
 import Block from './Block';
 import store, { StoreEvents } from './Store';
-import { Indexed } from './utils';
+import { Indexed, isEqual } from './utils';
 
 export function connect(Component: typeof Block, mapStateToProps: (state: Indexed) => Indexed) {
   return class extends Component {
     constructor(props: Record<string, unknown>) {
-      super({ ...props, ...mapStateToProps(store.getState()) });
+      let state = mapStateToProps(store.getState());
+      super({ ...props, ...state });
 
       store.on(StoreEvents.Updated, () => {
-        this.setProps({ ...mapStateToProps(store.getState()) });
+        const newState = mapStateToProps(store.getState());
+
+        if (!isEqual(state, newState)) {
+          this.setProps({ ...newState });
+        }
+        state = newState;
       });
     }
   };
