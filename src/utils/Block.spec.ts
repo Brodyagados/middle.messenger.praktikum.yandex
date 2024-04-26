0938194741dcf15b490df5ee4cad2356493b995a
Mezentsev Igor <schumm@mail.ru>
2024-04-26 12:31:22 +0500

    Удалил лишний код в тесте Block

--- src/utils/Block.spec.ts
+++ src/utils/Block.spec.ts
@@ -2,19 +2,11 @@ import { expect } from 'chai';
 import Block from './Block';
 
 describe('Тестирование Block', () => {
-    const testTemplate = '{{text}}';
-
-    interface ITestBlock {
-        text: string
-    }
+    const template = `{{text}}`;
 
     class TestBlock extends Block {
-        constructor(props: ITestBlock) {
-            super(props);
-        }
-
         render() {
-            return this.compile(testTemplate, this._props);
+            return this.compile(template, this._props);
         }
     }
 
