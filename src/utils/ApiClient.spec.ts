88a9bbc1e0c7554c4811b7120bc8615c6df71ded
Mezentsev Igor <schumm@mail.ru>
2024-04-27 07:53:44 +0500

    Начало работы над тестированием ApiClient

---
+++ src/utils/ApiClient.spec.ts
@@ -0,0 +1,26 @@
+import { SinonFakeXMLHttpRequest, SinonFakeXMLHttpRequestStatic, useFakeXMLHttpRequest } from 'sinon';
+import ApiClient, { HttpMethod } from './ApiClient';
+import { expect } from 'chai';
+
+describe('Тестирование ApiClient', () => {
+    let xhr: SinonFakeXMLHttpRequestStatic;
+    const requests: SinonFakeXMLHttpRequest[] = [];
+
+    beforeEach(() => {
+        xhr = useFakeXMLHttpRequest();
+        xhr.onCreate = (xhr: SinonFakeXMLHttpRequest) => {
+            requests.push(xhr);
+        };
+    });
+
+    afterEach(() => {
+        xhr.restore();
+    });
+
+    it('ApiClient.get() должен иметь тип запроса "GET"', () => {
+        ApiClient.get('/');
+
+        const [request] = requests;
+        expect(request.method).to.eq(HttpMethod.GET);
+    })
+});
